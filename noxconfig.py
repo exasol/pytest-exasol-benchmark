73069f14d144374dae584746ae96e49768c4dba0
Steffen Pankratz <steffen.pankratz@exasol.com>
2026-07-07 14:22:33 +0200

    Updated PTB to version 10.2.0 + fixed 'has_documentation' usage.
    Signed-off-by: Steffen Pankratz <steffen.pankratz@exasol.com>

--- noxconfig.py
+++ noxconfig.py
@@ -3,11 +3,22 @@ from __future__ import annotations
 from pathlib import Path
 
 from exasol.toolbox.config import BaseConfig
+from pydantic import computed_field
 
 
-PROJECT_CONFIG = BaseConfig(
+class Config(BaseConfig):
+    @computed_field  # type: ignore[misc]
+    @property
+    def has_documentation(self) -> bool:
+        """
+        Indicates that the project serves Sphinx-based documentation. With a few
+        exceptions, this should be the case for most projects.
+        """
+        return False
+
+
+PROJECT_CONFIG = Config(
     project_name="pytest_benchmark",
     root_path=Path(__file__).parent,
     python_versions=("3.10", "3.11", "3.12", "3.13", "3.14"),
-    has_documentation=False
 )
