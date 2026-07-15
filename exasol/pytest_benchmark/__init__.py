fab157757fc8c40995271492af3a357c0b06e4cb
Steffen Pankratz <steffen.pankratz@exasol.com>
2026-07-15 08:48:14 +0200

    Added benchmark + query cache context manager
    Signed-off-by: Steffen Pankratz <steffen.pankratz@exasol.com>

--- exasol/pytest_benchmark/__init__.py
+++ exasol/pytest_benchmark/__init__.py
@@ -1,3 +1,49 @@
+from contextlib import contextmanager
 from importlib.metadata import version
 
 __version__ = version("pytest_exasol_benchmark")
+import logging
+from collections.abc import Callable
+
+import pytest
+
+logger = logging.getLogger(__name__)
+
+
+def get_enable_query_cache_sql() -> str:
+    return "alter session set query_cache='on'"
+
+
+def get_disable_query_cache_sql() -> str:
+    return "alter session set query_cache='off'"
+
+
+@contextmanager
+def _disable_query_cache(query_func: Callable, disable_query_cache: bool):
+    if disable_query_cache:
+        query_func(get_disable_query_cache_sql())
+    yield
+    if disable_query_cache:
+        query_func(get_enable_query_cache_sql())
+
+
+@pytest.fixture
+def exasol_benchmark(benchmark, query_func: Callable):
+    def run_benchmark(
+        target: Callable,
+        args: tuple = (),
+        rounds=1,
+        warmup_rounds=0,
+        iterations=1,
+        disable_query_cache: bool = True,
+    ):
+        with _disable_query_cache(query_func, disable_query_cache):
+            return benchmark.pedantic(
+                target=target,
+                args=args,
+                rounds=rounds,
+                warmup_rounds=warmup_rounds,
+                iterations=iterations,
+            )
+
+    return run_benchmark
