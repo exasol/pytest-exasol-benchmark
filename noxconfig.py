e68d9667d9fc494199d329aab8c488e75a72c4c9
Steffen Pankratz <steffen.pankratz@exasol.com>
2026-07-24 11:49:04 +0200

    Enabled build and publication of documentation (fixes #7) (#8)
    Signed-off-by: Steffen Pankratz <steffen.pankratz@exasol.com>

--- noxconfig.py
+++ noxconfig.py
@@ -3,21 +3,8 @@ from __future__ import annotations
 from pathlib import Path
 
 from exasol.toolbox.config import BaseConfig
-from pydantic import computed_field
 
-
-class Config(BaseConfig):
-    @computed_field  # type: ignore[misc]
-    @property
-    def has_documentation(self) -> bool:
-        """
-        Indicates that the project serves Sphinx-based documentation. With a few
-        exceptions, this should be the case for most projects.
-        """
-        return False
-
-
-PROJECT_CONFIG = Config(
+PROJECT_CONFIG = BaseConfig(
     project_name="pytest_benchmark",
     root_path=Path(__file__).parent,
     python_versions=("3.10", "3.11", "3.12", "3.13"),
